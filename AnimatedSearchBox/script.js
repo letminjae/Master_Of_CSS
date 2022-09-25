7cfe01a7a33e73ffda75c1ad7efc18da525dfe22
letminjae <cmjj0824@naver.com>
2022-09-25 23:42:27 +0900

    ADD: Animated SearchBox

---
+++ AnimatedSearchBox/script.js
@@ -0,0 +1,11 @@
+let search = document.querySelector('.search');
+let close = document.querySelector('.close');
+let searchBox = document.querySelector('.searchBox');
+
+search.onclick = function(){
+    searchBox.classList.add('active');
+}
+
+close.onclick = function(){
+    searchBox.classList.remove('active');
+}
